99d7cc73f07fc831e293e9ea5940065634059d93
analirasgomez <145667803+analirasgomez@users.noreply.github.com>
2023-10-05 12:33:40 +0200

    a

---
+++ estudiantes.py
@@ -0,0 +1,8 @@
+grade = float (input ("Nota del estudiante sobre 100: "))
+assistance = int (input ("Clases atendidas: "))
+classes = int (input ("Clases de la asignatura totales"))
+cgrade = int ((assistance/classes)*100)
+if cgrade >= 80 and grade >=70 :
+    print ("Aprobado")
+else: 
+    print ("Suspenso")
---
+++ reverso.py
@@ -0,0 +1,5 @@
+sentence_1 = str (input("Escribe tu frase aqui:"))
+split_sentence = sentence_1.split()
+print (sentence_1)
+split_sentence .reverse()
+print (" ".join(split_sentence))
